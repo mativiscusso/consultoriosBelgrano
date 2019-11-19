3a9e8819e191a60b10fad5c5ecaf7b0b6975c9f1
mativiscusso <mativiscusso@gmail.com>
2019-11-19 13:37:42 -0300

    logo

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a id="logo" class="navbar-brand" href="../index.php">Consultorios Belgrano</a>
+            <a id="logo" class="navbar-brand" href="../index.php"><img src="img/logoBC.png" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
