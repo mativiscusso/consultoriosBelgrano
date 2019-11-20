74f745bb6a25dcf19216a48014dbbb09d09f9522
mativiscusso <mativiscusso@gmail.com>
2019-11-20 11:33:16 -0300

    cambios mapa

--- index.php
+++ index.php
@@ -161,8 +161,17 @@
                 </div>
             </div>
         </div>
-        <div class="container py-5">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.4103870886033!2d-60.71751338525432!3d-32.940172878889605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b6532c9d4f92e3%3A0x7bd635baee85c183!2sNuevos%20Consultorios%20Belgrano!5e0!3m2!1ses!2sar!4v1574174788651!5m2!1ses!2sar" width="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-6 py-5">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.4103870886033!2d-60.71751338525432!3d-32.940172878889605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b6532c9d4f92e3%3A0x7bd635baee85c183!2sNuevos%20Consultorios%20Belgrano!5e0!3m2!1ses!2sar!4v1574174788651!5m2!1ses!2sar" width="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                </div>
+                <div class="col-12 col-sm-6 py-5">
+                    <span class="display-3">¿DONDE</span><br>
+                    <span class="display-3">NOS</span><br>
+                    <span class="display-3">ENCONTRAS?</span>
+                </div>
+            </div>
         </div>
 
 
