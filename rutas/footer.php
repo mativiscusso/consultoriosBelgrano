ebc0b15d9f49c661abd9ee8d452776aa2901f391
mativiscusso <mativiscusso@gmail.com>
2019-11-20 09:24:42 -0300

    changes

--- rutas/footer.php
+++ rutas/footer.php
@@ -1,23 +1,24 @@
 <footer>
-    <div class="container-fluid py-2">
-        <div class="row">
-            <div class="col-12 col-sm-6">
-                <ul>
-                    <li>Mendoza 6924, S2000 Rosario, Santa Fe</li>
-                    <li>0341 456-4021 / 3416692938</li>
-                    <li>EMAIL</li>
-                    <li>Horario de Atención - 9hs a 21hs </li>
-                </ul>
-            </div>
-            <div class="col-12 col-md-6">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.4103870886033!2d-60.71751338525432!3d-32.940172878889605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b6532c9d4f92e3%3A0x7bd635baee85c183!2sNuevos%20Consultorios%20Belgrano!5e0!3m2!1ses!2sar!4v1574174788651!5m2!1ses!2sar"
-                    width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-            </div>
+    <div class="row">
+        <div class="col-12 col-sm-6 py-5">
+            <ul>
+                <li>Mendoza 6924, S2000 Rosario, Santa Fe</li>
+                <li>0341 456-4021 / 3416692938</li>
+                <li>info@belgranoconsultorios.com</li>
+                <li>Horario de Atención - 9hs a 21hs </li>
+            </ul>
+        </div>
+        <div class="col-12 col-sm-6 py-5">
+            <ul>
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>Whatsapp</li>
+            </ul>
         </div>
     </div>
-    
+    <span id="empresas">Esta web ha sido desarrollada por MAREA + Codear</span>
 </footer>
-<span id="empresas">Esta web ha sido desarrollada por MAREA + Codear</span>
+
 
 </body>
 
