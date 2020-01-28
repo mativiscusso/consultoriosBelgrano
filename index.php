4f011eb9fdb2a93429f726ce6a29497263e84c05
mativiscusso <mativiscusso@gmail.com>
2020-01-28 10:50:25 -0300

    barra redes

--- index.php
+++ index.php
@@ -53,7 +53,15 @@
     </header>
 
     <body>
-
+	<div class="social">
+		<ul>
+			<li><a href="http://www.facebook.com/falconmasters" target="_blank" class="icon-facebook"></a></li>
+			<li><a href="http://www.twitter.com/falconmasters" target="_blank" class="icon-twitter"></a></li>
+			<li><a href="http://www.googleplus.com/falconmasters" target="_blank" class="icon-googleplus"></a></li>
+			<li><a href="http://www.pinterest.com/falconmasters" target="_blank" class="icon-pinterest"></a></li>
+			<li><a href="mailto:contacto@falconmasters.com" class="icon-mail"></a></li>
+		</ul>
+	</div>
 
         <!-- <div id="carousel" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
