a334354b8351f46ef0bba56dba620303906f5689
mativiscusso <mativiscusso@gmail.com>
2020-01-28 10:18:52 -0300

    cambios

--- index.php
+++ index.php
@@ -16,9 +16,10 @@
 </head>
 
 <body>
+<div id="portada" class="container-fluid">
     <header>
-        <nav id="barraMenu" class="navbar navbar-expand-lg navbar-light fixed-top">
-            <a id="logo" class="navbar-brand" href="index.php"><img src="img/logoBC.png" alt=""></a>
+        <nav id="barraMenu" class="navbar navbar-expand-lg navbar-light">
+            <a id="logo" class="navbar-brand" href="index.php"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -54,7 +55,7 @@
     <body>
 
 
-        <div id="carousel" class="carousel slide" data-ride="carousel">
+        <!-- <div id="carousel" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -79,6 +80,13 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
+        </div> -->
+        
+        <div>
+        <!-- <img class="logo"src="img/logoBC.png" alt=""> -->
+<!--         <h1>BELGRANO</h1>
+        <h1>CONSULTORIOS</h1> -->
+        </div>
         </div>
 
         <div class="container py-5">
@@ -167,19 +175,8 @@
                 <div class="col-12 col-sm-6 py-5">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.4103870886033!2d-60.71751338525432!3d-32.940172878889605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b6532c9d4f92e3%3A0x7bd635baee85c183!2sNuevos%20Consultorios%20Belgrano!5e0!3m2!1ses!2sar!4v1574174788651!5m2!1ses!2sar" width="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
                 </div>
-                <div class="col-12 col-sm-6 py-5">
-                    <span class="display-3">¿DONDE</span><br>
-                    <span class="display-3">NOS</span><br>
-                    <span class="display-3">ENCONTRAS?</span>
-                </div>
             </div>
         </div>
-
-
-
-
-
-
 
 
         <?php
